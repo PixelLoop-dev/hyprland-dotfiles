b647cb011aec01e40d953d6a400226c1fe93dd72
Katsuro <udalovmatvej69@gmail.com>
2025-07-23 02:13:37 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo Установка пакетов pacman:
+echo Installing pacman packages
 sudo pacman -S --needed git kitty firefox wofi obs-studio fish rofi steam nwg-look nvim fastfetch spotify-launcher pavucontrol btop cava swww telegram-desktop openrgb nautilus wine visual-studio-code-bin  hyprshot hyprpicker
 
 
-echo Установка yay
+echo Installing yay
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si 
 cd .. && rm -rf yay
 
 
-echo Установка пакетов yay
+echo Installing yay packages
 yay -S vesktop nekoray waypaper waybar-cava 
 
 
-echo Установка шрифтов
+echo Installing fonts
 sudo pacman -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-liberation ttf-droid ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-font-awesome
 
 
-echo Копирование конфигов
+echo Copying configs
 mkdir -p ~/.config ~/.themes ~/.icons ~/.Wallpapers
 
 cp -rn .themes/* ~/.themes/
@@ -27,21 +27,14 @@ cp -rn .config/* ~/.config/
 cp -rn .Wallpapers/* ~/.Wallpapers/
 
 
-echo Изменение шелла на fish
+echo Changing shell to fish
 chsh -s /usr/bin/fish
 
 set -U fish_color_cwd 57A3C9
 set -U fish_color_user 57A3C9
 
-echo Установка темы VSCode
+echo Installing a VSCode Theme
 code --install-extension enkia.tokyo-night
 
-echo Установка завершена.
+echo \nTo complete the installation, reboot your system
 
-read -p "Перезапустить систему? (y/n): " choice
-if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-    echo "Перезагрузка системы..."
-    sudo reboot
-else
-    echo "Установка завершена."
-fi
