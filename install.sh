c33f6f13b070e623146246b42dc9c7736cae0164
Katsuro <udalovmatvej69@gmail.com>
2025-07-22 15:16:04 +0400

    vscode theme added

--- install.sh
+++ install.sh
@@ -3,16 +3,20 @@
 echo Установка пакетов pacman:
 sudo pacman -S --needed git kitty firefox wofi obs-studio fish rofi steam nwg-look nvim fastfetch spotify-launcher pavucontrol btop cava swww telegram-desktop openrgb nautilus wine visual-studio-code-bin  hyprshot hyprpicker
 
+
 echo Установка yay
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si 
 cd .. && rm -rf yay
 
+
 echo Установка пакетов yay
 yay -S vesktop nekoray waypaper waybar-cava 
 
+
 echo Установка шрифтов
 sudo pacman -S --needed noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-liberation ttf-droid ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-font-awesome
+
 
 echo Копирование конфигов
 mkdir -p ~/.config ~/.themes ~/.icons ~/.Wallpapers
@@ -22,12 +26,15 @@ cp -rn .icons/* ~/.icons/
 cp -rn .config/* ~/.config/
 cp -rn .Wallpapers/* ~/.Wallpapers/
 
+
 echo Изменение шелла на fish
 chsh -s /usr/bin/fish
 
 set -U fish_color_cwd 57A3C9
 set -U fish_color_user 57A3C9
 
+echo Установка темы VSCode
+code --install-extension enkia.tokyo-night
 
 echo Установка завершена.
 
